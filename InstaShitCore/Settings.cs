2b2ecb9501d92d5fb3b71a341f5cf37de234316b
Konrad Krawiec <konrad11901@gmail.com>
2018-11-29 23:22:25 +0100

    Don't answer marketing questions by default

--- InstaShitCore/Settings.cs
+++ InstaShitCore/Settings.cs
@@ -13,7 +13,7 @@ namespace InstaShitCore
         public List<List<IntelligentMistakesDataEntry>> IntelligentMistakesData { get; set; }
         public bool AllowTypo { get; set; } = true;
         public bool AllowSynonym { get; set; } = true;
-		public bool AnswerMarketingQuestions { get; set; } = true;
+		public bool AnswerMarketingQuestions { get; set; } = false;
         public bool Debug { get; set; } = false;
     }
 }
